07009376c4f1073c32ed2e34eda995d3cdd1d731
zaideim <95437612+zaideim@users.noreply.github.com>
2023-08-28 22:46:50 -0600

    PortafolioM2.py

--- PortafolioM2.py
+++ PortafolioM2.py
@@ -14,7 +14,8 @@ import math
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 # Modelo KNN
 # Distancia euclidiana entre dos puntos
@@ -23,6 +24,7 @@ def euclidean_distance(point1, point2):
     for i in range(len(point1)):
         squared_distance += (point1[i] - point2[i]) ** 2
     return math.sqrt(squared_distance)
+
 # Vecinos más cercanos
 def predict_knn(training_data, test_point, k):
     distances = []
@@ -56,7 +58,7 @@ def predict_knn(training_data, test_point, k):
 data = []
 with open('Student_Performance.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
-    next(csvreader) 
+    next(csvreader)
     for row in csvreader:
         hours_studied = int(row[0])
         previous_scores = int(row[1])
@@ -66,11 +68,40 @@ with open('Student_Performance.csv', 'r') as csvfile:
         performance_index = float(row[5])
         data.append([hours_studied, previous_scores, extracurricular, sleep_hours, question_papers, performance_index])
 
-# Dividir los datos en conjuntos de entrenamiento y prueba
-train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+train_data, test_data = train_test_split(data, test_size=0.3, random_state=42)
 
-#Hacer predicciones
-k = 3 
+# Búsqueda de hiperparámetros para encontrar la cantidad correcta de vecinos
+best_k = None
+best_accuracy = 0.0
+
+for k_candidate in range(1,200):
+    current_accuracies = []
+
+    for test_point in test_data:
+        predicted_label = predict_knn(train_data, test_point, k_candidate)
+        true_label = test_point[-1]
+        accuracy = 1 if predicted_label == true_label else 0
+        current_accuracies.append(accuracy)
+
+    current_average_accuracy = sum(current_accuracies) / len(current_accuracies)
+
+    if current_average_accuracy > best_accuracy:
+        best_accuracy = current_average_accuracy
+        best_k = k_candidate
+
+    print(f"Para k = {k_candidate}, la precisión promedio es: {current_average_accuracy}")
+
+print(f"El valor de k que produce la mayor precisión promedio es: {best_k}")
+
+# Usar el valor óptimo de k para las predicciones finales
+final_accuracies = []
+
 for test_point in test_data:
-    predicted_label = predict_knn(train_data, test_point, k)
+    predicted_label = predict_knn(train_data, test_point, best_k)
+    true_label = test_point[-1]
+    accuracy = 1 if predicted_label == true_label else 0
+    final_accuracies.append(accuracy)
     print(f"Para el punto {test_point}, la predicción es: {predicted_label}")
+
+final_average_accuracy = sum(final_accuracies) / len(final_accuracies)
+print(f"Precisión promedio final: {final_average_accuracy}")
